ba9b1ad3b267003c7a0c186447536c8ee9ff8efb
RizkiAshari <rizkiashari1026@gmail.com>
2022-07-09 22:00:26 +0700

    Add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Logo from '../assets/images/logo.png'
+import FlagEN from '../assets/icon/EN.png'
+import FlagID from '../assets/icon/ID.png'
 
 const Header = () => {
   return (
-    <nav>
-      <div className='container'>Home</div>
+    <nav className='py-6'>
+      <div className='container flex justify-between items-center'>
+        <img src={Logo} alt='logo' className='w-52' />
+        <div className='flex items-center justify-between'>
+          <ul className='flex items-center justify-between'>
+            <li className='text-nav-inActive px-4 py-2 mr-4'>Home</li>
+            <li className='text-nav-inActive px-4 py-2 mr-4'>About Us</li>
+            <li className='text-nav-inActive px-4 py-2 mr-4'>Visi & Mission</li>
+            <li className='text-nav-inActive px-4 py-2 mr-4'>Services</li>
+            <li className='text-nav-inActive px-4 py-2 mr-4'>Contact Us</li>
+          </ul>
+          <select className='ml-12'>
+            <option value='en'>
+              <img src={FlagEN} alt='flag-en' /> EN
+            </option>
+            <option value='id'>
+              <img src={FlagID} className='rounded' alt='flag-en' /> ID
+            </option>
+          </select>
+        </div>
+      </div>
     </nav>
   )
 }
