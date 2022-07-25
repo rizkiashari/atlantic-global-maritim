8496c7bae46cdaad026c5874319f68d0feff429c
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-07-25 14:12:00 +0700

    update navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,31 +2,60 @@ import React from 'react'
 import Logo from '../assets/images/logo.png'
 import FlagEN from '../assets/icon/EN.png'
 import FlagID from '../assets/icon/ID.png'
+import { Dropdown, Navbar } from 'flowbite-react'
 
 const Header = () => {
   return (
-    <nav className='py-6'>
-      <div className='container flex justify-between items-center'>
+    <Navbar fluid={true} rounded={true}>
+      <Navbar.Brand href='/'>
         <img src={Logo} alt='logo' className='w-52' />
-        <div className='flex items-center justify-between'>
-          <ul className='flex items-center justify-between'>
-            <li className='text-nav-inActive px-4 py-2 mr-4'>Home</li>
-            <li className='text-nav-inActive px-4 py-2 mr-4'>About Us</li>
-            <li className='text-nav-inActive px-4 py-2 mr-4'>Visi & Mission</li>
-            <li className='text-nav-inActive px-4 py-2 mr-4'>Services</li>
-            <li className='text-nav-inActive px-4 py-2 mr-4'>Contact Us</li>
-          </ul>
-          <select className='ml-12'>
-            <option value='en'>
-              <img src={FlagEN} alt='flag-en' /> EN
-            </option>
-            <option value='id'>
-              <img src={FlagID} className='rounded' alt='flag-en' /> ID
-            </option>
-          </select>
-        </div>
+      </Navbar.Brand>
+      <div className='flex md:order-2'>
+        <Dropdown
+          label={
+            <p className='flex gap-3 items-center'>
+              <img src={FlagEN} alt='flag-en' className='w-7 h-5' />
+              EN
+            </p>
+          }
+          color='white'
+        >
+          <Dropdown.Item>
+            <p className='flex gap-3 items-center'>
+              <img src={FlagEN} alt='flag-en' className='w-7 h-5' />
+              EN
+            </p>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <p className='flex gap-3 items-center'>
+              <img src={FlagID} alt='flag-en' className='w-7 h-5' />
+              ID
+            </p>
+          </Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
       </div>
-    </nav>
+      <Navbar.Collapse>
+        <Navbar.Link href='/' active={true}>
+          <p>Home</p>
+        </Navbar.Link>
+        <Navbar.Link href='/about-us'>
+          <p className='text-nav-inActive'>About Us</p>
+        </Navbar.Link>
+        <Navbar.Link href='/visi-misi'>
+          <p className='text-nav-inActive'>Visi & Mission</p>
+        </Navbar.Link>
+        <Navbar.Link href='/services'>
+          <p className='text-nav-inActive'>Services</p>
+        </Navbar.Link>
+        <Navbar.Link href='/facilities'>
+          <p className='text-nav-inActive'>Facilities</p>
+        </Navbar.Link>
+        <Navbar.Link href='/contact-us'>
+          <p className='text-nav-inActive'>Contact Us</p>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
