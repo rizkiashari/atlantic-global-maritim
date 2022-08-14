c183aa2de2d7340354228c9c3e50d72ed77e92d1
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-08-14 20:50:21 +0700

    Done google map

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,6 @@ import { useLoadScript, GoogleMap, Marker } from '@react-google-maps/api'
 const Footer = () => {
   const lang = localStorage.getItem('lang')
 
-  // console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
-
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   })
@@ -75,7 +73,7 @@ const Footer = () => {
           <h3 className='text-white text-sm md:text-base lg:text-lg xl:text-xl font-semibold mb-5'>
             {lang === 'id' ? 'Peta Lokasi' : ' Map Location'}
           </h3>
-          {!isLoaded ? (
+          {/* {!isLoaded ? (
             <div>Loading...</div>
           ) : (
             <GoogleMap
@@ -87,16 +85,14 @@ const Footer = () => {
                 position={{ lat: -6.236631390735907, lng: 107.08383048390209 }}
               />
             </GoogleMap>
-          )}
-          {/* <iframe
+          )} */}
+          <iframe
             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.203694413507!2d107.08160961476925!3d-6.236860695485393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698f6e69c018f9%3A0x751b2e0d978f9478!2sPermata%20Regensi!5e0!3m2!1sid!2sid!4v1660484175906!5m2!1sid!2sid'
-            width='400'
-            height='300'
-            className='border-none'
-            allowfullscreen=''
+            title='map footer'
+            className='border-none md:w-52 w-full h-full'
             loading='lazy'
             referrerpolicy='no-referrer-when-downgrade'
-          ></iframe> */}
+          ></iframe>
         </div>
       </div>
     </div>
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import EmailContact from '../components/EmailContact'
 import Footer from '../components/Footer'
+import { useLoadScript, GoogleMap, Marker } from '@react-google-maps/api'
 
 const ContactUs = () => {
   const lang = localStorage.getItem('lang')
@@ -9,6 +10,10 @@ const ContactUs = () => {
   useEffect(() => {
     document.title = 'Atlantic Global Maritim | Contact Us'
   }, [])
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  })
 
   return (
     <>
@@ -21,7 +26,26 @@ const ContactUs = () => {
         </div>
       </div>
       <EmailContact />
-      <p>Google Map Here</p>
+      {/* {!isLoaded ? (
+        <div>Loading...</div>
+      ) : (
+        <GoogleMap
+          zoom={10}
+          center={{ lat: -6.236631390735907, lng: 107.08383048390209 }}
+          mapContainerClassName='map-contacus'
+        >
+          <Marker
+            position={{ lat: -6.236631390735907, lng: 107.08383048390209 }}
+          />
+        </GoogleMap>
+      )} */}
+      <iframe
+        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.203694413507!2d107.08160961476925!3d-6.236860695485393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698f6e69c018f9%3A0x751b2e0d978f9478!2sPermata%20Regensi!5e0!3m2!1sid!2sid!4v1660484175906!5m2!1sid!2sid'
+        title='map contact us'
+        className='border-none w-full h-[80vh]'
+        loading='lazy'
+        referrerpolicy='no-referrer-when-downgrade'
+      ></iframe>
       <Footer />
     </>
   )
