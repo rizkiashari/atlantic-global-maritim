0f4c3e4416f820350ddd7399cfccca03dbbe2030
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-08-14 20:43:42 +0700

    Done Google map in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react'
 import LogoFooter from '../assets/icon/logo-footer.webp'
 import EmailIcon from '../assets/icon/gmail-icon.webp'
 import WebIcon from '../assets/icon/website.webp'
+import { useLoadScript, GoogleMap, Marker } from '@react-google-maps/api'
 
 const Footer = () => {
   const lang = localStorage.getItem('lang')
+
+  // console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
 
   return (
     <div className='footer-hero md:px-14 px-6 md:py-6 py-4 text-white'>
@@ -13,7 +21,7 @@ const Footer = () => {
         className='w-40 h-36 object-cover mb-5'
         alt='Logo'
       />
-      <div className='flex md:flex-wrap flex-nowrap md:flex-row sm:flex-row flex-col gap-5 justify-between items-start'>
+      <div className='flex flex-wrap md:flex-nowrap md:flex-row sm:flex-row flex-col gap-5 justify-between items-start'>
         <div>
           <h3 className='text-white text-sm md:text-base lg:text-lg xl:text-xl font-semibold mb-5'>
             {lang === 'id' ? 'Alamat kami' : 'Our Address'}
@@ -67,7 +75,28 @@ const Footer = () => {
           <h3 className='text-white text-sm md:text-base lg:text-lg xl:text-xl font-semibold mb-5'>
             {lang === 'id' ? 'Peta Lokasi' : ' Map Location'}
           </h3>
-          {/* Here Map */}
+          {!isLoaded ? (
+            <div>Loading...</div>
+          ) : (
+            <GoogleMap
+              zoom={10}
+              center={{ lat: -6.236631390735907, lng: 107.08383048390209 }}
+              mapContainerClassName='map-footer'
+            >
+              <Marker
+                position={{ lat: -6.236631390735907, lng: 107.08383048390209 }}
+              />
+            </GoogleMap>
+          )}
+          {/* <iframe
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.203694413507!2d107.08160961476925!3d-6.236860695485393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698f6e69c018f9%3A0x751b2e0d978f9478!2sPermata%20Regensi!5e0!3m2!1sid!2sid!4v1660484175906!5m2!1sid!2sid'
+            width='400'
+            height='300'
+            className='border-none'
+            allowfullscreen=''
+            loading='lazy'
+            referrerpolicy='no-referrer-when-downgrade'
+          ></iframe> */}
         </div>
       </div>
     </div>
